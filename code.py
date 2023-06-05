174c18bfeda2a27c924a8ba9c8bbdc457ef54a25
Preston <prestonchildress@gmail.com>
2023-06-05 14:36:12 -0400

    Added eStop, switched to key presses
    Tested with 23.0 Driver Statioin

--- code.py
+++ code.py
@@ -2,10 +2,12 @@ import os
 import usb_hid
 from adafruit_hid.keyboard import Keyboard
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
 import socketpool
 import wifi
 import mdns
 from adafruit_httpserver import Server, Request, Response, GET
+import time
 
 mdns.Server(wifi.radio).hostname = os.getenv("TEAM")
 
@@ -17,12 +19,25 @@ keyboard_layout = KeyboardLayoutUS(keyboard)
 
 @server.route("/enable", GET)
 def sendEnable(request: Request):
-    keyboard_layout.write("[]\\")
+    keyboard.press(Keycode.LEFT_BRACKET)
+    keyboard.press(Keycode.RIGHT_BRACKET)
+    keyboard.press(Keycode.BACKSLASH)
+    time.sleep(0.1)
+    keyboard.release_all()
     return Response(request, "Enabled")
 
 @server.route("/disable", GET)
 def sendDisable(request: Request):
-    keyboard_layout.write("\n")
+    keyboard.press(Keycode.ENTER)
+    time.sleep(0.1)
+    keyboard.release_all()
     return Response(request, "Disabled")
+
+@server.route("/estop", GET)
+def sendEStop(request: Request):
+    keyboard.press(Keycode.SPACEBAR)
+    time.sleep(0.1)
+    keyboard.release_all()
+    return Response(request, "Estopped")
 
 server.serve_forever(str(wifi.radio.ipv4_address))
